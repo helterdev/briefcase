828d9c9a6f782f7de85a6eb03f7147ee9e2e8382
Helter <heltherv@gmail.com>
2023-10-13 19:47:23 -0500

    Feature: Frontend tool links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,13 +8,13 @@ const Footer = () => {
                     <h3>Say Hello</h3>
                     <ul className="contact">
                         <li>
-                            <a href="https://www.linkedin.com/in/helter-conforme-velez-59848218b/">LinkedIn</a>
+                            <a href="https://www.linkedin.com/in/helter-conforme-velez-59848218b/" target="_blank">LinkedIn</a>
                         </li>
                         <li>
-                            <a href="https://github.com/helterdev">Helterdev</a>
+                            <a href="https://github.com/helterdev" target="_blank">Helterdev</a>
                         </li>
                         <li>
-                            <a href="mailto: heltherv@gmail.com">heltherv@gmail.com</a>
+                            <a href="mailto: heltherv@gmail.com" target="_blank">heltherv@gmail.com</a>
                         </li>
 
                     </ul>
--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -19,53 +19,79 @@ const Tools = () => {
                 <h2 className="tools-description__title">Tools</h2>
                 <ul className="tools-description__container">
                     <ToolsMotion label={"html"}>
+                    <a href="https://developer.mozilla.org/es/docs/Web/HTML" target="_blank">
                         <AiFillHtml5 className="tools-icons tools-icons--html"/>
                         <p>HTML</p>
+                    </a>
                     </ToolsMotion>
                     <ToolsMotion label={"css"}>
+                    <a href="https://developer.mozilla.org/es/docs/Web/CSS" target="_blank">
                         <DiCss3 className="tools-icons tools-icons--css"/>
                         <p>css</p>
+                    </a>
                     </ToolsMotion>
                     <ToolsMotion>
+                    <a href="https://developer.mozilla.org/es/docs/Web/JavaScript" target="_blank">
                         <BiLogoJavascript className="tools-icons tools-icons--javascript"/>
                         <p>javascript</p>
+                    </a>
                     </ToolsMotion>
                     <ToolsMotion>
+                    <a href="https://es.react.dev/" target="_blank">
                         <BiLogoReact className="tools-icons tools-icons--react"/>
                         <p>react</p>
+                    </a>
                     </ToolsMotion>
                     <ToolsMotion>
+                    <a href="https://redux-toolkit.js.org/" target="_blank">
                         <BiLogoRedux className="tools-icons tools-icons--redux"/>
                         <p>reduxtookit</p>
+                    </a>
                     </ToolsMotion>
                     <ToolsMotion>
+                    <a href="https://sass-lang.com/" target="_blank">
                         <DiSass className="tools-icons tools-icons--sass"/>
                         <p>sass</p>
+                    </a>
                     </ToolsMotion>
                     <ToolsMotion>
+                    <a href="https://tailwindcss.com/" target="_blank">
                         <BiLogoTailwindCss className="tools-icons tools-icons--tailwind"/>
                         <p>taildwindcss</p>
+                    </a>
                     </ToolsMotion>
                     <ToolsMotion>
+                    <a href="https://www.typescriptlang.org/" target="_blank">
                         <BiLogoTypescript className="tools-icons tools-icons--typescript"/>
                         <p>typescript</p>
+                    </a>
                     </ToolsMotion>
                     <ToolsMotion>
+                    <a href="https://git-scm.com/" target="_blank">
                         <BiLogoGit className="tools-icons tools-icons--git"/>
                         <p>git</p>
+                    </a>
                     </ToolsMotion>
                     <ToolsMotion>
+                    <a href="https://github.com/" target="_blank">
                         <BiLogoGithub className="tools-icons tools-icons--github"/>
                         <p>gitHub</p>
+                    </a>
                     </ToolsMotion>
                     <ToolsMotion>
+                    <a href="https://www.figma.com/" target="_blank">
                         <BiLogoFigma className="tools-icons tools-icons--figma"/>
                         <p>figma</p>
+                    </a>
                     </ToolsMotion>
+                    
                     <ToolsMotion>
+                    <a href="https://trello.com/home" target="_blank">
                         <BiLogoTrello className="tools-icons tools-icons--trello"/>
                         <p>trello</p>
+                    </a>
                     </ToolsMotion>
+                    
                 </ul>
             </div>
             </div>
